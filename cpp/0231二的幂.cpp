58ece3bebfcb1c5dcb5fdbf221c187d81b252a75
xiaoyu2018 <861900161@qq.com>
2022-05-12 11:32:48 +0800

    0231

--- cpp/0231二的幂.cpp
+++ cpp/0231二的幂.cpp
@@ -5,38 +5,42 @@ using namespace std;
 
 bool isPowerOfTwo1(int n)
 {
-    unordered_set<int> s = {1,
-                            2,
-                            4,
-                            8,
-                            16,
-                            32,
-                            64,
-                            128,
-                            256,
-                            512,
-                            1024,
-                            2048,
-                            4096,
-                            8192,
-                            16384,
-                            32768,
-                            65536,
-                            131072,
-                            262144,
-                            524288,
-                            1048576,
-                            2097152,
-                            4194304,
-                            8388608,
-                            16777216,
-                            33554432,
-                            67108864,
-                            134217728,
-                            268435456,
-                            536870912,
-                            1073741824};
-    return s.count(n);
+    switch (n)
+    {
+    case 1:
+    case 2:
+    case 4:
+    case 8:
+    case 16:
+    case 32:
+    case 64:
+    case 128:
+    case 256:
+    case 512:
+    case 1024:
+    case 2048:
+    case 4096:
+    case 8192:
+    case 16384:
+    case 32768:
+    case 65536:
+    case 131072:
+    case 262144:
+    case 524288:
+    case 1048576:
+    case 2097152:
+    case 4194304:
+    case 8388608:
+    case 16777216:
+    case 33554432:
+    case 67108864:
+    case 134217728:
+    case 268435456:
+    case 536870912:
+    case 1073741824:
+        return true;
+    }
+    return false;
 }
 
 bool isPowerOfTwo2(int n)

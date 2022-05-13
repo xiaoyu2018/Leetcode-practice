52997348f23f165b25aa995fb583853cee7b1166
xiaoyu2018 <861900161@qq.com>
2022-05-13 21:56:02 +0800

    🦁

--- python/temp.py
+++ python/temp.py
@@ -1,16 +1,32 @@
-nums = [3,2,2,3]
-val = 3
+from functools import lru_cache
 
-def rm():
-    slow=fast=0
+@lru_cache(None)
+def fibo1(n:int):
+    if(n in [1,2]):
+        return 1
 
-    while(fast<len(nums)):
-        if(nums[fast]!=val):
-            nums[slow]=nums[fast]
-            slow+=1
-        fast+=1
+    return fibo1(n-1)+fibo1(n-2)
+
+
+memo=dict()
+def fibo2(n:int):
+    if(n in [1,2]):
+        memo[n]=1
+
+    if(n in memo.keys()):
+        return memo[n]
     
-    return slow
+    val=fibo2(n-1)+fibo2(n-2)
+    memo[n]=val
 
-print(rm())
-print(nums)
+    return val
+
+# print(fibo2(999))
+
+def createArray2D(h,w):
+    return [[0]*w for j in range(h)]
+    # return [[0]*w]*h 错误写法，低维数组地址相同
+
+a=createArray2D(3,4)
+a[0][0]=1
+print(a)

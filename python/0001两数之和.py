36708e97bb104b649db1ee344f7b5ba08db6e1fe
xiaoyu2018 <861900161@qq.com>
2022-05-27 20:14:21 +0800

    0001

--- python/0001两数之和.py
+++ python/0001两数之和.py
@@ -1,23 +1,12 @@
-test_nums=[3,3]
-test_target=6
-# 暴力
-def TwoSum1(nums:list,target:int) -> list:
-    nums_len=len(nums)
-    for i in range(nums_len):
-        for j in range(i+1,nums_len):
-            if(nums[i]+nums[j]==target):
-                return [i,j]
 
-# 引入字典
-def TwoSum2(nums:list,target:int) -> list:
-    nums_len=len(nums)
-    map={}
+def twoSum(nums: list, target: int):
 
-    for i in range(nums_len):
-        j=map.get(target-nums[i])
-        if(j!=None):
-            return [i,j]
-        map[nums[i]]=i
-
-
-print(TwoSum2(test_nums,test_target))
+    memo=dict()
+    memo[nums[0]]=0
+    for i,e in enumerate(nums[1:],1):
+        
+        if(target-e in memo.keys()):
+            return [i,memo[target-e]]
+        
+        memo[e]=i
+    

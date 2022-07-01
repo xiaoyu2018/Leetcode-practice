ee28c882625ed99233ecea86118766e747c3e685
xiaoyu2018 <861900161@qq.com>
2022-07-01 12:17:14 +0800

    094

--- cpp/094二叉树的中序遍历.cpp
+++ cpp/094二叉树的中序遍历.cpp
@@ -1,5 +1,6 @@
 #include "mybitree.h"
 #include <vector>
+#include<stack>
 
 // 递归
 void Traversal(TreeNode *root, vector<int> &res)
@@ -12,13 +13,36 @@ void Traversal(TreeNode *root, vector<int> &res)
     }
 }
 
+void Traversal2(TreeNode *root, vector<int> &res)
+{
+    stack<TreeNode *> st;
+    TreeNode *cur = root;
+    while (cur != NULL || !st.empty())
+    {
+        if (cur != NULL)
+        {                    // 指针来访问节点，访问到最底层
+            st.push(cur);    // 将访问的节点放进栈
+            cur = cur->left; // 左
+        }
+        else
+        {
+            cur = st.top(); // 从栈里弹出的数据，就是要处理的数据（放进result数组里的数据）
+            st.pop();
+            res.push_back(cur->val);    // 中
+            cur = cur->right;           // 右
+        }
+    }
+    
+}
+
 vector<int> inorderTraversal(TreeNode *root)
 {
     vector<int> res;
-    Traversal(root, res);
+    Traversal2(root, res);
 
     return res;
 }
+
 
 int main()
 {
--- python/094二叉树的中序遍历.py
+++ python/094二叉树的中序遍历.py
@@ -1,7 +1,9 @@
 from binarytree import *
 
-def inorderTraversal(root: TreeNode) -> list:
+def inorderTraversal1(root: TreeNode) -> list:
     res=[]
+    if(not root):
+        return res
     def _iot(r:TreeNode):
 
         if(r):
@@ -12,3 +14,30 @@ def inorderTraversal(root: TreeNode) -> list:
     _iot(root)
     return res
 
+
+
+def inorderTraversal2(root: TreeNode) -> list:
+    res=[]
+    
+    if(not root):
+        return res
+    
+    st=[]
+    p=root
+
+    while(st or p):
+        
+        if(p):
+            st.append(p)
+            p=p.left
+        
+        else:
+            p=st[-1]
+            st.pop()
+            res.append(p.val)
+            p=p.right
+        
+    return res
+
+
+print(inorderTraversal2(pre_create([1,None,2,3])))

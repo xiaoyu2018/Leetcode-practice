c2dbac792483bcb72b075e3aab9846d40c6f10e1
xiaoyu2018 <861900161@qq.com>
2022-09-05 09:47:05 +0800

    0047

--- python/0047全排列Ⅱ.py
+++ python/0047全排列Ⅱ.py
@@ -1,3 +1,29 @@
 
 def permuteUnique(nums: list) -> list:
-    ...
+    res=[]
+    temp=[]
+    d_used=[0]*8
+
+    def _backTracking():
+        nonlocal nums
+
+        b_used=[0]*21
+
+        if(len(nums)==len(temp)):
+            res.append(temp.copy())
+            return
+        
+        for i,v in enumerate(nums):
+
+            if(d_used[i] or b_used[v+10]):
+                continue
+            
+            d_used[i]=~d_used[i]
+            b_used[v+10]=~b_used[v+10]
+            temp.append(v)
+            _backTracking()
+            d_used[i]=~d_used[i]
+            temp.pop()
+
+    _backTracking()
+    return res

4e705955bf370547089d615a276772e6aab16806
xiaoyu2018 <861900161@qq.com>
2022-09-26 20:56:52 +0800

    0343

---
+++ cpp/0343整数拆分.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+using namespace std;
+
+int integerBreak(int n)
+{
+    vector<int> dp(n + 1);
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++)
+    {
+        for (int j = 1; j < i - 1; j++)
+        {
+            dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+        }
+    }
+    return dp[n];
+}
---
+++ python/0343整数拆分.py
@@ -0,0 +1,17 @@
+
+
+# dp[i]为数字i能拆分成的最大乘积
+# 
+def integerBreak(n: int) -> int:
+
+    dp=[0]*(n+1)
+    dp[2]=1
+
+    for i in range(3,n+1):
+
+        for j in range(1,i-1):
+            dp[i]=max(dp[i],j*(i-j),j*dp[i-j])
+
+    return dp[n]
+
+print(integerBreak(10))

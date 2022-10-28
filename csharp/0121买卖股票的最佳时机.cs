21992b733e5b78d303a0ee4d5b493dc40e4526c3
xiaoyu2018 <861900161@qq.com>
2022-10-28 11:01:44 +0800

    0121

--- csharp/0121买卖股票的最佳时机.cs
+++ csharp/0121买卖股票的最佳时机.cs
@@ -1,7 +1,20 @@
 
 public partial class Solution
 {
-    public int MaxProfit(int[] prices)
+    public int MaxProfit3(int[] prices)
+    {
+        int min = Int32.MaxValue;
+
+        int res = 0;
+        for (int i = 0; i < prices.Length; i++)
+        {
+            min = Math.Min(prices[i], min);
+            res = Math.Max(prices[i] - min, res);
+        }
+        return res;
+    }
+
+    public int MaxProfit4(int[] prices)
     {
         int[] dp = new int[2];
         int size = prices.Length;

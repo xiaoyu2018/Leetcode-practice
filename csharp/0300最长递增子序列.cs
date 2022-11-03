556545ba9d6832a0093157ccebaa433368b48b36
xiaoyu2018 <861900161@qq.com>
2022-11-03 16:10:45 +0800

    0300

--- csharp/0300最长递增子序列.cs
+++ csharp/0300最长递增子序列.cs
@@ -3,24 +3,21 @@ public partial class Solution
 {
     public int LengthOfLIS(int[] nums)
     {
-        int res = 1;
         int size = nums.Length;
 
         int[] dp = new int[size];
         for (int i = 0; i < size; i++)
             dp[i] = 1;
-
+        
         for (int i = 1; i < size; i++)
         {
             for (int j = 0; j < i; j++)
             {
-                if (nums[i] > nums[j])
+                if(nums[i]>nums[j])
                     dp[i] = Math.Max(dp[i], dp[j] + 1);
             }
-
-            res = Math.Max(res, dp[i]);
         }
 
-        return res;
+        return dp.Max();
     }
 }
--- python/0300最长递增子序列.py
+++ python/0300最长递增子序列.py
@@ -1,13 +1,15 @@
+
+# dp，注意不是最长连续递增
 def lengthOfLIS(nums: list) -> int:
-    size = len(nums)
-    dp = [1] * size
+    size=len(nums)
 
-    for i in range(1, size):
+    dp=[1]*size
+
+    for i in range(1,size):
+
         for j in range(i):
-            if nums[i] > nums[j]:
-                dp[i] = max(dp[i], dp[j] + 1)
-
+            if(nums[i]>nums[j]):
+                dp[i]=max(dp[i],dp[j]+1)
+    
     return max(dp)
-
-
 print(lengthOfLIS([1, 3, 6, 7, 9, 4, 10, 5, 6]))

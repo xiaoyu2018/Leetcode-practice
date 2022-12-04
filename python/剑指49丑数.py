1047eb61ffbc5fd318e8bb601faf5347d781d811
xiaoyu2018 <861900161@qq.com>
2022-12-04 16:14:25 +0800

    剑指49

---
+++ python/剑指49丑数.py
@@ -0,0 +1,22 @@
+
+# dp，后面的丑数等于前面的丑数*(2/3/5)
+# dp[i]为第i个丑数
+def nthUglyNumber(n: int) -> int:
+
+    dp=[1]*(n+1)
+    p2=p3=p5=1
+    for i in range(2,n+1):
+        num1,num2,num3=dp[p2]*2,dp[p3]*3,dp[p5]*5
+        
+        dp[i]=min(num1,num2,num3)
+
+        if(dp[i]==num1):
+            p2+=1
+        if(dp[i]==num2):
+            p3+=1
+        if(dp[i]==num3):
+            p5+=1
+    
+    return dp[-1]
+
+print(nthUglyNumber(10))

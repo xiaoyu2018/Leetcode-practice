9b6aac63f70dee05ec2efae96a81dfe9a960f6f3
xiaoyu2018 <861900161@qq.com>
2023-02-08 21:03:40 +0800

    剑指68

--- python/剑指68二叉树的最近公共祖先.py
+++ python/剑指68二叉树的最近公共祖先.py
@@ -16,19 +16,14 @@ def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
     
     _get_parents(root)
 
-    p_parents=[p]
-    q_parents=[q]
+    visited=set()
     
     while(p!=None):
-        p_parents.append(parents[p])
+        visited.add(p.val)
         p=parents[p]
-    while(q!=None):
-        q_parents.append(parents[q])
-        q=parents[q]
-    p_parents.pop()
-    q_parents.pop()
     
-    for i in p_parents:
-        for j in q_parents:
-            if(i.val==j.val):
-                return i
+    while(q!=None):
+        if(q.val in visited):
+            return q
+        q=parents[q]
+    

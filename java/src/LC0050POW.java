491af16232260f1b032995ba7ed198b68d6d49b6
xiaoyu2018 <861900161@qq.com>
2023-03-14 15:45:31 +0800

    LC0050

---
+++ java/src/LC0050POW.java
@@ -0,0 +1,23 @@
+
+public class LC0050POW {
+    private double _calc(double x, long n)
+    {
+        if(n==0)
+            return 1;
+        double res = _calc(x, n / 2);
+        res *= res;
+        if (n % 2 == 1)
+            res *= x;
+        return res;
+    }
+
+    public double myPow(double x, int n) 
+    {
+        boolean flag = n > 0;
+        long N = n;
+        double res = flag?_calc(x, N): _calc(x, -N);
+        
+        return flag ? res : 1 / res;
+    }
+
+}

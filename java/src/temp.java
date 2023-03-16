eb017c7803eeb2f602f518d0cd98c8f3b4986f91
xiaoyu2018 <861900161@qq.com>
2023-03-16 20:27:36 +0800

    😭

--- java/src/temp.java
+++ java/src/temp.java
@@ -1,18 +1,53 @@
 public class temp {
 
     public void foo1(String... strings) {
-        Person p = new Person(null, 0);
-        Student s = new Student(null, 0, null);
-        Person pp = s;
+        // Person p = new Person(null, 0);
+        // Student s = new Student(null, 0, null);
+        // Person pp = s;
 
-        Student ss = pp instanceof Student ? (Student) pp : null;
-        Student.foo();
-
+        // Student ss = pp instanceof Student ? (Student) pp : null;
+        // Student.foo();
+        // Outer o1 = new Outer("1");
+        // System.out.println(o1.getField());
+        // Outer.Inner o2 = o1.new Inner("2");
+        // System.out.println(o2.field);
+        // System.out.println(o1.getField());
+        // Integer i1 = 2;
+        // i1 = 3;
+        // int x = -1;
+        // assert x > 1;
+        System.out.println(111);
     }
 }
 
+
+
 interface Ishit {
     static String filed1 = "111";
+}
+
+class Outer
+{
+    private String field;
+
+    public Outer(String string) {
+        field = string;
+    }
+
+    public String getField()
+    {
+        return field;
+    }
+    
+    class Inner
+    {
+        public String field;
+        
+        public Inner(String field) {
+            Outer.this.field = "decrepted";
+            this.field=field;
+        }    
+    }
 }
 
 class Person 
@@ -37,4 +72,6 @@ class Student extends Person
         super(name, age);
         this.ID = id;
     }
+    
+    
 }

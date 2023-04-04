fc80b84c4d0dbe67b6c0e371444fd6871139ab2e
xiaoyu2018 <861900161@qq.com>
2023-04-04 16:40:17 +0800

    🥲

--- python/test/test_0001两数之和.py
+++ python/test/test_0001两数之和.py
@@ -1,6 +1,29 @@
 import sys
-sys.path.append("..")
+from importlib import import_module
+sys.path.append("/mnt/d/WSL/Ubuntu/code/leetcode/python")
+target=import_module("main.0001两数之和")
+import pytest
 
-from main import 0001两数之和
-def test_foo():
+
+def test_twoSum():
+    assert target.twoSum([1,2,5],3) in [[1,0],[0,1]]
+
+@pytest.mark.demo
+def test_demo1():
+    assert True
+
+@pytest.mark.demo
+def test_demo2():
+    assert False
+
+@pytest.mark.skipif(True,reason="test")
+def test_skip1():
+    assert False
+@pytest.mark.skipif(True,reason="test")
+
+def test_skip2():
+    assert False
+@pytest.mark.skipif(True,reason="not implemented")
+def test_skip():
     ...
+

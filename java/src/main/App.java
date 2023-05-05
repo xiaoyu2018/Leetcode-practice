dfa5815d2f3e5e485ed1423bd8c8951d53c583cc
xiaoyu2018 <861900161@qq.com>
2023-05-05 14:03:14 +0800

    HW412_1

--- java/src/main/App.java
+++ java/src/main/App.java
@@ -1,12 +1,22 @@
 package main;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import main.algorithm.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        String[] nums = sc.nextLine().strip().split(" ");
+        int target = sc.nextInt();
         
+        int[] revokes = new int[nums.length];
+        int i=0;
+        for (String n : nums) {
+            revokes[i++] = Integer.parseInt(n);
+        }
         
+        System.out.println(new HW412_1().solve(revokes, target));
     }
 }
---
+++ java/src/main/algorithm/HW412_1.java
@@ -0,0 +1,33 @@
+package main.algorithm;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class HW412_1 {
+    
+    private boolean _check(int limit,int[] revokes,int target)
+    {
+        int s = 0;
+        for (int num : revokes) {
+            s += num > limit ? limit : num;
+        }
+        return s<=target;
+    }
+
+    public int solve(int[] revokes,int target)
+    {
+        //二分
+        int m=Arrays.stream(revokes).max().getAsInt();
+        int l = 0, r = m;
+        while(l<=r)
+        {
+            int mid = l + ((r - l) >> 1);
+            if (_check(mid, revokes, target))
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        // 0 1 2
+        return r == m ? -1 : r;
+    }
+}

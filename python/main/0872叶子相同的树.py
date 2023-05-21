ef8378a7afa79c4c15d7fa293cf28658f647ebe5
xiaoyu2018 <861900161@qq.com>
2023-05-21 19:35:26 +0800

    0872

--- python/main/0872叶子相同的树.py
+++ python/main/0872叶子相同的树.py
@@ -1,20 +1,23 @@
 from binarytree import *
 
-def leafSimilar(root1: TreeNode, root2: TreeNode) -> bool:
-    leaves1=[]
-    leaves2=[]
 
-    def _bfs(root:TreeNode,leaves:list):
-        if(root.left==None and root.right==None):
+def leafSimilar(root1: TreeNode, root2: TreeNode) -> bool:
+    leaves1 = []
+    leaves2 = []
+
+    def _bfs(root: TreeNode, leaves: list):
+        if root == None:
+            return
+        if root.left == None and root.right == None:
             leaves.append(root.val)
             return
-        
-        _bfs(root.left,leaves)                
-        _bfs(root.right,leaves)
 
-    _bfs(root1,leaves1)
-    _bfs(root2,leaves2)
+        _bfs(root.left, leaves)
+        _bfs(root.right, leaves)
+
+    _bfs(root1, leaves1)
+    _bfs(root2, leaves2)
 
     print(leaves1)
     print(leaves2)
-    return leaves1==leaves2
+    return leaves1 == leaves2

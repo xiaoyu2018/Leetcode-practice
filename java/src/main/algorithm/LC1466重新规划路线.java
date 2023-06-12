8a4cec80a116ebf39d7579f879b2b4a2ba538e5b
xiaoyu2018 <861900161@qq.com>
2023-06-12 18:06:20 +0800

    LC1466

--- java/src/main/algorithm/LC1466重新规划路线.java
+++ java/src/main/algorithm/LC1466重新规划路线.java
@@ -1,9 +1,7 @@
 package main.algorithm;
 
 import java.util.ArrayDeque;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
@@ -13,36 +11,28 @@ public class LC1466重新规划路线 {
         Queue<Integer> nodes = new ArrayDeque<>();
         int size = connections.length;
         Set<Integer> memo = new HashSet<>();
-        Map<Integer, Integer> to = new HashMap<>();
-        Map<Integer, Integer> from = new HashMap<>();
+        int[][] matrix = new int[n][n];
 
         for (int i = 0; i < size; i++) {
-            to.put(connections[i][1], connections[i][0]);
-            from.put(connections[i][0], connections[i][1]);
+            matrix[connections[i][0]][connections[i][1]] = 1;
         }
         nodes.add(0);
-        memo.add(0);
 
         while (!nodes.isEmpty()) {
-            int crt_val = nodes.poll();
+            int crt = nodes.poll();
 
-            if (to.containsKey(crt_val)) {
-                int tmp = to.get(crt_val);
-                if (!memo.contains(tmp)) {
-                    nodes.add(tmp);
-                    memo.add(tmp);
-                }
-            }
-            
-            if (from.containsKey(crt_val)) {
-                int tmp= from.get(crt_val);
-                if (!memo.contains(tmp)) {
-                    nodes.add(tmp);
-                    memo.add(tmp);
+            memo.add(crt);
+            for (int i = 0; i < n; i++) {
+                if (memo.contains(i))
+                    continue;
+                if (matrix[crt][i] == 1) {
                     res++;
+                    nodes.add(i);
+                }
+                if (matrix[i][crt] == 1) {
+                    nodes.add(i);
                 }
             }
-
         }
 
         return res;
